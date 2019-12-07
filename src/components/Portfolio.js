c4b8453b3375dab2adf0895ecc52b36cd1f996a7
Emily <raper003@umn.edu>
2019-12-07 12:43:45 -0600

    first initial basic layout, transferred from previous project.  Backend of app mostly created.

---
+++ src/components/Portfolio.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>My Projects</h1>
+          <h2>Web Development</h2><br></br>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h2>Technical and Professional Writing</h2><br></br>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h2>Information and Graphic Design</h2><br></br>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "",
+    "role": "",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "Small description here",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"",
+    "address":"",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"",
+        "specialization":"",
+        "GraduationMonth":"",
+        "GraduationYear":"",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"",
+        "specialization":"",
+        "GraduationMonth":"",
+        "GraduationYear":"",
+        "Achievements":"Some Achievements Here"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Role",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":""
+      },
+      {
+        "skillname":""
+      },
+      {
+        "skillname":""
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"",
+        "imgurl":""
+      },
+      {
+        "name":"project2",
+        "description":"",
+        "imgurl":""
+      },
+      {
+        "name":"project3",
+        "description":"",  
+        "imgurl":""
+      },
+      {
+        "name":"project4",
+        "description":"",
+        "imgurl":""
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"",
+        "name":""
+      },
+      {
+        "description":"",
+        "name":""
+      }
+    ]
+  }
+  
+  export default resumeData
