63c624c729136ba6abe6021650519f142585200f
Emily <raper003@umn.edu>
2019-12-30 13:20:49 -0600

    update contact section

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -8,8 +8,9 @@ export default class ContactUs extends Component {
               <p className="lead">
               <h2>Contact</h2><br></br>
 
-              Emily Raper </p>
-           <p>   763.381.1098 | raper003@umn.edu            </p>
+              Emily Raper<br></br>Greater Minneapolis-St. Paul Area<br></br> 763.381.1098 | <a href="mailto: raper003@umn.edu" target="_blank" rel="noopener noreferrer">raper003@umn.edu</a>
+
+              </p>          
             </div>
           </div>
           <div className="row">
