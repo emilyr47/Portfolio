04b77899326451b77236aa442960e96444e1919c
Emily <raper003@umn.edu>
2020-06-12 10:46:46 -0500

    build changes

---
+++ build/precache-manifest.c7e4597f95794046312ea81f17f0cf6f 2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "044b1ad6216bf940d99a",
+    "url": "/portfolio/build/index.html/static/css/main.0ceaa07f.chunk.css"
+  },
+  {
+    "revision": "044b1ad6216bf940d99a",
+    "url": "/portfolio/build/index.html/static/js/main.044b1ad6.chunk.js"
+  },
+  {
+    "revision": "63a8f196648076aa1c3d",
+    "url": "/portfolio/build/index.html/static/js/1.63a8f196.chunk.js"
+  },
+  {
+    "revision": "6b9ef858904a47daeec4",
+    "url": "/portfolio/build/index.html/static/js/runtime~main.6b9ef858.js"
+  },
+  {
+    "revision": "48b841011f0e3b055c0bbc3942bc72bd",
+    "url": "/portfolio/build/index.html/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.602f0ba09afc6a6b60cc7c19206b6c9e.js"
+  "/portfolio/build/index.html/precache-manifest.c7e4597f95794046312ea81f17f0cf6f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/build/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
