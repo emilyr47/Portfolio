5af693bb5469ba9efe9691aa07109b482cffbce4
Emily <raper003@umn.edu>
2020-06-12 10:15:32 -0500

    Updates

---
+++ precache-manifest.a6ee38a9621faaf06697c50f32c5c46f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fd3ae373afbe04261fd",
+    "url": "/portfolio/static/css/main.0ceaa07f.chunk.css"
+  },
+  {
+    "revision": "2fd3ae373afbe04261fd",
+    "url": "/portfolio/static/js/main.2fd3ae37.chunk.js"
+  },
+  {
+    "revision": "63a8f196648076aa1c3d",
+    "url": "/portfolio/static/js/1.63a8f196.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "379127624f49aafd7f89c83fd5ab2277",
+    "url": "/portfolio/index.html"
+  }
+];
